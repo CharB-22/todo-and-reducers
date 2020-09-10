0f036d988cbfcbf373eca7d54ed7f42c1ad46fc9
Charlene <c.bennevault@gmail.com>
2020-09-10 14:59:24 +0200

    Split into 2 contexts

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -5,13 +5,16 @@ import useTodoState from "../hooks/useTodoState";
 const initialTodos = [{ id: 1, task: "Walk the dog", completed: true }];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodoProvider(props){
     const [todos, dispatch] = useReducer(reducer, initialTodos);
     
     return(
-        <TodosContext.Provider value={{todos, dispatch}}>
-            {props.children}
+        <TodosContext.Provider value={todos}>
+            <DispatchContext.Provider value={dispatch}>
+                {props.children}
+            </DispatchContext.Provider>
         </TodosContext.Provider>
     )
 }
