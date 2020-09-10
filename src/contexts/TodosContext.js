9d4fa598758a3971129b61b8a3ac54e939e8dbe0
Charlene <c.bennevault@gmail.com>
2020-09-10 19:09:55 +0200

    Add memo for optimization

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -1,6 +1,5 @@
 import React, {createContext, useReducer} from "react";
 import reducer from "../reducers/todo.reducer"
-import useTodoState from "../hooks/useTodoState";
 
 const initialTodos = [{ id: 1, task: "Walk the dog", completed: true }];
 
